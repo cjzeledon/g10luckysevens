ac67ddad8a95469e006b69be2cea96123772611e
Claudia Zeledon <cjzeledon@gmail.com>
2018-09-20 09:35:53 -0400

    Fixed logic for rollCountToHighestAmountHeld

--- lucky.js
+++ lucky.js
@@ -26,11 +26,9 @@ function playGame() {
     const startingBet = document.forms["gameForm"]["startingBet"].value;
     var money = parseInt(startingBet);
     const tallyMoney = [];
-
-    // Testing these codes
-    const tallyMoneyWon = [];
-    const tallyMoneyLose = [];
-    var totalRolls = 0;
+    // Add the startingBet to the beginning of array of tallyMoney
+    // And will return a new length
+    tallyMoney.unshift(money);
 
     while (money > 0) {
       var dice1 = rollTheDice();
@@ -38,56 +36,56 @@ function playGame() {
       var rolledDice = dice1 + dice2;
 
       if (rolledDice != 7) {
-        totalRolls++
         money -= 1;
         tallyMoney.push(money);
-        tallyMoneyLose.push(money);
       } else {
-        totalRolls++
         money += 4;
         tallyMoney.push(money);
-        tallyMoneyWon.push(money);
       }
     }
 
     // Keeps track of how many rolls were taken
     var tallyRolls = tallyMoney.length;
 
-    // Keeps track of the maximum amount of money held by the user
+    // Keeps track of the total amount of money held by the user
     const add = (a, b) => a + b;
     const sumofMoney = tallyMoney.reduce(add);
 
     // Keeps track of how many rolls were taken at the point when the user held the most money
     const heldMostMoney = Math.max.apply(null, tallyMoney);
-    var getIndexOfTallyMoney = tallyMoney.indexOf(heldMostMoney);
-    var rollCountToHighestAmount = getIndexOfTallyMoney + 1;
 
-    console.log("Starting Bet: " + startingBet);
-    console.log("Current Bet: " + money);
-    console.log("Array of All Money Held: " + tallyMoney);
-    console.log("Array of Money Won: " + tallyMoneyWon);
-    console.log("Array of Money Lost: " + tallyMoneyLose);
-    console.log("First dice is: " + dice1);
-    console.log("Second dice is: " + dice2);
-    console.log("Sum of rolled dice is: " + rolledDice);
-    console.log("Total Rolls Before Going Broke: " + tallyRolls);
-    console.log("Highest Amount Won: " + sumofMoney);
-    console.log("Held Most Money: " + heldMostMoney);
-    console.log("Index of Tally Money Array That Held Most Money: " + getIndexOfTallyMoney);
-    console.log("Roll Count At Highest Amount Won: " + rollCountToHighestAmount);
-    console.log("Total Rolls: " + totalRolls);
+    // Gets the index of the highest money held
+    var getIndexOfTallyMoney = tallyMoney.indexOf(heldMostMoney);
+
+    // NOTE: If the highest amount held was the starting money, shouldn't the roll count be 0?
+    if (heldMostMoney == startingBet) {
+      var rollCountToHighestAmountHeld = getIndexOfTallyMoney;
+    } else {
+      rollCountToHighestAmountHeld = getIndexOfTallyMoney + 1;
+    }
 
     function showResults() {
       document.getElementById("results").style.visibility = "visible";
       document.getElementById("resultsStartingBet").textContent = "$" + startingBet;
       document.getElementById("resultsTotalRollBeforeBroke").textContent = tallyRolls;
-      document.getElementById("resultsHighestAmountWon").textContent = "$" + heldMostMoney;
-      document.getElementById("resultsRollCountHighestAmountWon").textContent = rollCountToHighestAmount;
-
-      // Change to calling the element tag instead of its Id since it "should" only be called on once, not twice
-      document.getElementById("play").textContent = "Play Again";
-      document.getElementById("play").type = "reset";
+      document.getElementById("resultsHighestAmountHeld").textContent = "$" + heldMostMoney;
+      document.getElementById("resultsRollCountHighestAmountHeld").textContent = rollCountToHighestAmountHeld;
+      document.getElementsByTagName("button").textContent = "Play Again";
+      document.getElementsByTagName("button").type = "reset";
     }
+
     showResults();
+
+    console.log("Starting Bet: " + startingBet);
+    console.log("Current Bet: " + money);
+    console.log("Array of All Money Held: " + tallyMoney);
+    console.log("First dice is: " + dice1);
+    console.log("Second dice is: " + dice2);
+    console.log("Sum of rolled dice is: " + rolledDice);
+    console.log("Total Rolls Before Going Broke: " + tallyRolls);
+    console.log("Total Sum of Array of All Money Held: " + sumofMoney);
+    console.log("Held Most Money: " + heldMostMoney);
+    console.log("Index of Tally Money Array That Held Most Money: " + getIndexOfTallyMoney);
+    console.log("Roll Count At Highest Amount Held: " + rollCountToHighestAmountHeld);
   }
 }
